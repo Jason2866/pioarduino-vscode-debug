dac30221364614470c0252bc84aecbdf9e349c4a
Jason2866 <obermeier.johann@googlemail.com>
2026-03-11 15:58:26 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+'use strict';
+const path = require('path');
+
+/**@type {import('webpack').Configuration[]}*/
+module.exports = [
+    {
+        name: 'extension',
+        target: 'node',
+        entry: './src/extension.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'extension.js',
+            libraryTarget: 'umd',
+            globalObject: 'global',
+            library: 'platformio-vscode-debug',
+        },
+        externals: {
+            vscode: 'vscode',
+            'copy-paste': 'copy-paste',
+            'vscode-debugadapter': 'vscode-debugadapter',
+            xml2js: 'xml2js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader',
+                },
+            ],
+        },
+        devtool: false,
+    },
+    {
+        name: 'adapter',
+        target: 'node',
+        entry: './src/backend/adapter.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'adapter.js',
+            libraryTarget: 'umd',
+            globalObject: 'global',
+            library: 'platformio-vscode-debug',
+        },
+        externals: {
+            vscode: 'vscode',
+            'copy-paste': 'copy-paste',
+            'vscode-debugadapter': 'vscode-debugadapter',
+            xml2js: 'xml2js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader',
+                },
+            ],
+        },
+        devtool: false,
+    },
+];
